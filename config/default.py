e4b1c0cc6f834ae0da0e45b440729cec71faa469
whiteUnicorn <bdh931101@gmail.com>
2016-04-22 03:18:13 +0900

    Update file config

--- config/default.py
+++ config/default.py
@@ -6,16 +6,28 @@ class Config(object):
     APP_NAME = 'hims_guest'
     ROOT_DIR = os.path.dirname(os.getcwd())
 
+    ALLOWED_EXTENSIONS = ['jpg', 'jpeg', 'png']
+    FILE_CONTENT_TYPES = { # these will be used to set the content type of S3 object. It is binary by default.
+        'jpg': 'image/jpeg',
+        'jpeg': 'image/jpeg',
+        'png': 'image/png'
+    }
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/hims_guest.db'
     DEBUG = False
     LOG_LEVEL = 'info'
     MOD = 'PRO'
+    BASE_SERVER = 'http://localhost:8080'
+    API_SERVER = BASE_SERVER + '/api/'
+    FILE_SERVER = BASE_SERVER + '/file/'
 
 class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/hims_guest.db'
     DEBUG = True
     LOG_LEVEL = 'debug'
     MOD = 'DEV'
-    
+    BASE_SERVER = 'http://localhost:8080'
+    API_SERVER = BASE_SERVER + '/api/'
+    FILE_SERVER = BASE_SERVER + '/file/'
+
