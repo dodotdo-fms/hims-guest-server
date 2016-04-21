378f1f441f8f10cd0ebffaac2a689e4f550ce097
whiteUnicorn <bdh931101@gmail.com>
2016-04-22 05:40:19 +0900

    Add requirements.txt

--- run.py
+++ run.py
@@ -3,5 +3,5 @@
 from hims_guest import app, db
 
 if __name__ == '__main__':
-    db.create_all(app)
-    app.run(host="0.0.0.0", port=8080, debug=True, threaded=True)
+    db.create_all(app=app)
+    app.run(host="0.0.0.0", port=8082, debug=True, threaded=True)
